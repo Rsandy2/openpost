0995efc68fd8dcaba32b1837ebc75b2e8639c746
Rsandy2 <raesandy2@gmail.com>
2020-11-21 22:38:29 -0500

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,22 @@
-<<<<<<< HEAD:src/components/App.js
 import React, { useState, useEffect, useRef } from "react";
-import Card from "./Card.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/Loading";
 
-function App() {
-  return <div className="App"></div>;
-}
-=======
-import React, { useState, useEffect, useRef } from "react"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Loading from './components/Loading'
->>>>>>> main:src/App.js
-
-const Home = React.lazy(() => import('./components/Home'))
-const Quotes = React.lazy(() => import('./components/Quotes'))
-const Create = React.lazy(() => import('./components/create-quote/Create'))
+const Home = React.lazy(() => import("./components/Home"));
+const Quotes = React.lazy(() => import("./components/Quotes"));
+const Create = React.lazy(() => import("./components/create-quote/Create"));
 
 export default function App() {
   return (
     <Router>
-      <React.Suspense fallback = { <Loading /> }>
+      <React.Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path = '/' component = { Home } />
-          <Route exact path = '/quotes' component = { Quotes } />
-          <Route exact path = '/create' component = { Create } />
-          <Route render = {() => <h1>404 not found</h1> } />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/quotes" component={Quotes} />
+          <Route exact path="/create" component={Create} />
+          <Route render={() => <h1>404 not found</h1>} />
         </Switch>
       </React.Suspense>
     </Router>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import "./index.css";
-import App from "./components/App";
-=======
 import "./css/index.css";
 import App from "./App";
->>>>>>> main
 
 ReactDOM.render(<App />, document.getElementById("root"));
