0ec7cee3b3fe41509b47594692eb5cbdfeac3d40
Eric Gamarra <eric.gamarra01@gmail.com>
2020-11-22 00:33:07 -0800

    Removed UNNCESSARY STUFF added by Raejae

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react"
+import React from "react"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
 
